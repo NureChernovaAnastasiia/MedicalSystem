7a9b04b3bdbd4403f792106bb8067e8127c1b264
Anastasiia Chernova <anastasiia.chernova@nure.ua>
2025-04-20 22:56:20 +0300

    Add hospital staff routes with /doctors and /medical-staff filters

--- server/routes/hospitalStaffRoutes.js
+++ server/routes/hospitalStaffRoutes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const hospitalStaffController = require('../controllers/hospitalStaffController');
 
-router.get('/', hospitalStaffController.getAll);
+// STATIC routes first
+router.get('/doctors', hospitalStaffController.getDoctors);
+router.get('/medical-staff', hospitalStaffController.getMedicalStaff);
+
+// DYNAMIC route after
 router.get('/:id', hospitalStaffController.getById);
+router.get('/', hospitalStaffController.getAll);
 router.post('/', hospitalStaffController.create);
 router.put('/:id', hospitalStaffController.update);
 router.delete('/:id', hospitalStaffController.delete);
