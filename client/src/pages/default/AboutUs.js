84c5346f37ab4013b23f864c315dd55360a1f211
DarinaBuravkova <daryna.buravkova@nure.ua>
2025-04-23 23:13:21 +0200

    Refactored the code of the About us page markup file

--- client/src/pages/default/AboutUs.js
+++ client/src/pages/default/AboutUs.js
@@ -7,6 +7,25 @@ import iconReduce from '../../img/icons/reduce.png';
 import iconSecure from '../../img/icons/secure.png';
 import '../../style/AboutUs.css';
 
+const FeatureCard = ({ icon, title, description }) => (
+  <div className="feature">
+    <div className="feature-icon">
+      <img src={icon} alt={title} />
+    </div>
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </div>
+);
+
+const ValueCard = ({ title, description }) => (
+  <div className="value-item">
+    <div className="value-circle">
+      <h3>{title}</h3>
+    </div>
+    <p className="value-description">{description}</p>
+  </div>
+);
+
 const AboutUs = () => {
   return (
     <div className="about-page">
@@ -38,68 +57,30 @@ const AboutUs = () => {
       </section>
 
       <section className="features">
-        <div className="feature">
-          <div className="feature-icon">
-            <img src={iconLaptop} alt="Технологічність" />
-          </div>
-          <h3>Технологічність</h3>
-          <p>Використовуємо найсучасніші IT-рішення для швидкої та безпечної роботи</p>
-        </div>
-        <div className="feature">
-          <div className="feature-icon">
-            <img src={iconReduce} alt="Доступність" />
-          </div>
-          <h3>Доступність</h3>
-          <p>Пацієнти можуть легко записатися на прийом та отримати результати аналізів онлайн</p>
-        </div>
-        <div className="feature">
-          <div className="feature-icon">
-            <img src={iconSecure} alt="Безпека" />
-          </div>
-          <h3>Безпека</h3>
-          <p>Захищені сервери та шифрування гарантують безпеку ваших даних</p>
-        </div>
+        <FeatureCard icon={iconLaptop} title="Технологічність" description="Використовуємо найсучасніші IT-рішення для швидкої та безпечної роботи" />
+        <FeatureCard icon={iconReduce} title="Доступність" description="Пацієнти можуть легко записатися на прийом та отримати результати аналізів онлайн" />
+        <FeatureCard icon={iconSecure} title="Безпека" description="Захищені сервери та шифрування гарантують безпеку ваших даних" />
       </section>
 
       <section className="mission">
         <div className="header-box">
           <div className="rectangle">
-          <h2>Наша місія та цінності</h2>
+            <h2>Наша місія та цінності</h2>
           </div>
         </div>
         <p className="quote">"Робити медицину простішою, швидшою та доступнішою для всіх!"</p>
         <div className="values">
-          <div className="value-item">
-            <div className="value-circle">
-              <h3>Інновації</h3>
-            </div>
-            <p className="value-description">Ми постійно вдосконалюємо платформу для зручності користувачів</p>
-          </div>
-          <div className="value-item">
-            <div className="value-circle">
-              <h3>Якість</h3>
-            </div>
-            <p className="value-description">Ми співпрацюємо лише з кваліфікованими лікарями</p>
-          </div>
-          <div className="value-item">
-            <div className="value-circle">
-              <h3>Конфіденційність</h3>
-            </div>
-            <p className="value-description">Ваші медичні дані під надійним захистом</p>
-          </div>
-          <div className="value-item">
-            <div className="value-circle">
-              <h3>Підтримка</h3>
-            </div>
-            <p className="value-description">Ми завжди готові допомогти у вирішенні будь-яких питань</p>
-          </div>
+          <ValueCard title="Інновації" description="Ми постійно вдосконалюємо платформу для зручності користувачів" />
+          <ValueCard title="Якість" description="Ми співпрацюємо лише з кваліфікованими лікарями" />
+          <ValueCard title="Конфіденційність" description="Ваші медичні дані під надійним захистом" />
+          <ValueCard title="Підтримка" description="Ми завжди готові допомогти у вирішенні будь-яких питань" />
         </div>
       </section>
 
       <section className="why-lifeline">
         <div className="header-box">
           <div className="rectangle">
-          <h2>Чому LifeLine – це правильний вибір?</h2>
+            <h2>Чому LifeLine – це правильний вибір?</h2>
           </div>
         </div>
         <div className="content">
