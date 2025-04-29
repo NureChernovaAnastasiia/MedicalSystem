740f7ad65f254557e84dafd498ad1779ccdc70cb
Anastasiia Chernova <anastasiia.chernova@nure.ua>
2025-04-30 00:33:38 +0300

    feat(routes): add routes for creating and fetching reviews

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
+const authMiddleware = require('../middleware/authMiddleware');
+const reviewValidationMiddleware = require('../middleware/reviewValidationMiddleware');
 
+// Без авторизації
+router.get('/target/:target_type/:target_id', reviewController.getByTarget);
 router.get('/', reviewController.getAll);
 router.get('/:id', reviewController.getById);
-router.post('/', reviewController.create);
-router.put('/:id', reviewController.update);
-router.delete('/:id', reviewController.delete);
+
+// Захищені маршрути
+router.post('/', authMiddleware, reviewValidationMiddleware, reviewController.create);
+router.put('/:id', authMiddleware, reviewController.update);
+router.delete('/:id', authMiddleware, reviewController.delete);
 
 module.exports = router;
