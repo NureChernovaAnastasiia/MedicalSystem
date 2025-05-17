70f462c4ff6ac8176a685506002bde5a50c9457e
Anastasiia Chernova <anastasiia.chernova@nure.ua>
2025-05-17 23:08:39 +0300

    Add routes for lab test access and report generation

--- server/routes/labTestRoutes.js
+++ server/routes/labTestRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const labTestController = require('../controllers/labTestController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.use(authMiddleware);
 
 router.get('/', labTestController.getAll);
 router.get('/:id', labTestController.getById);
