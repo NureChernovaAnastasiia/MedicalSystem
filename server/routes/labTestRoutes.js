1259243d2d218d82e063f6ce1df72c617a101554
Anastasiia Chernova <anastasiia.chernova@nure.ua>
2025-05-21 23:00:09 +0300

    Add API endpoint to mark lab tests as ready via PATCH /api/lab-tests/mark-ready/:id

--- server/routes/labTestRoutes.js
+++ server/routes/labTestRoutes.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const labTestController = require('../controllers/labTestController');
-const authMiddleware = require('../middleware/authMiddleware');
+const labTestController = require("../controllers/labTestController");
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/checkRoleMiddleware");
 
 router.use(authMiddleware);
 
-router.get('/', labTestController.getAll);
-router.get('/:id', labTestController.getById);
-router.get('/patient/:patientId', labTestController.getByPatient);
-router.get('/status/filter', labTestController.getByPatientStatus);
-router.post('/', labTestController.create);
-router.put('/:id', labTestController.update);
-router.delete('/:id', labTestController.delete);
-router.get('/:id/pdf', labTestController.downloadPDF);
+router.patch(
+  "/mark-ready/:id",
+  roleMiddleware ("Admin", "Doctor"),
+  labTestController.markReadyStatus
+);
+
+router.get("/", labTestController.getAll);
+router.get("/:id", labTestController.getById);
+router.get("/patient/:patientId", labTestController.getByPatient);
+router.get("/status/filter", labTestController.getByPatientStatus);
+router.post("/", labTestController.create);
+router.put("/:id", labTestController.update);
+router.delete("/:id", labTestController.delete);
+router.get("/:id/pdf", labTestController.downloadPDF);
 
 module.exports = router;
