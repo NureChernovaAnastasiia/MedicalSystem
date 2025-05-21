2d02706d5e791026b6f2bfeb23f44d38860e192d
DarinaBuravkova <daryna.buravkova@nure.ua>
2025-05-21 17:47:28 +0200

    Connected API to display patient comments on the main page of the website

--- client/src/http/reviewAPI.js
+++ client/src/http/reviewAPI.js
@@ -1,4 +1,4 @@
-import { $authHost } from "./index"; 
+import { $host, $authHost } from "./index"; 
 
 export const submitReview = async (reviewData) => {
   try {
@@ -6,6 +6,16 @@ export const submitReview = async (reviewData) => {
     return data;
   } catch (error) {
     console.error("Помилка при надсиланні відгуку:", error);
+    throw error;
+  }
+};
+
+export const fetchAllReviews = async () => {
+  try {
+    const { data } = await $host.get('api/reviews');
+    return data;
+  } catch (error) {
+    console.error("Помилка при отриманні коментарів:", error);
     throw error;
   }
 };
