8ea209cf07749469960ba2b1da3fd9e4f813c8ae
DarinaBuravkova <daryna.buravkova@nure.ua>
2025-05-26 23:50:42 +0200

    Created a modal component for patient registration and a page for entering patient data, implemented the registration logic and connected the API

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -12,6 +12,7 @@ export const DOCTOR_SERVICES_ROUTE = '/work-services';
 export const DOCTOR_SERVICESRESULT_ROUTE = '/work-services-results';
 export const DOCTOR_HOSPITALDETAILS_ROUTE = '/work-hospital';
 export const DOCTOR_MEDDETAIL_ROUTE = '/patient-meddetail';
+export const DOCTOR_FILLPATDATA_ROUTE = '/edit-data';
 export const PATIENT_PANEL_ROUTE = '/patpanel';
 export const ADMIN_PANEL_ROUTE = '/adminpanel';
 export const PATIENT_MEDCARD_ROUTE = '/medcard';
