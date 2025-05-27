80c89c1651da15308e594879aafc6a0921115aaf
Anastasiia Chernova <anastasiia.chernova@nure.ua>
2025-05-27 22:39:06 +0300

    Add API to fetch all patients of a specific hospital for Admin and Doctor roles

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -1,32 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const patientController = require('../controllers/patientController');
-const authMiddleware = require('../middleware/authMiddleware');
-const checkRole = require('../middleware/checkRoleMiddleware');
+const patientController = require("../controllers/patientController");
+const authMiddleware = require("../middleware/authMiddleware");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
 router.use(authMiddleware);
 
-router.put("/:id/photo", authMiddleware, checkRole("Patient"), patientController.updatePhoto);
+// Get all patients of a specific hospital (Doctor, Admin)
+router.get(
+  "/by-hospital/:hospitalId",
+  checkRole("Doctor", "Admin"),
+  patientController.getByHospital
+);
+
+router.put(
+  "/:id/photo",
+  authMiddleware,
+  checkRole("Patient"),
+  patientController.updatePhoto
+);
 // Отримати пацієнта за user_id
-router.get('/by-user/:userId', checkRole('Admin', 'Doctor', 'Patient'), patientController.getByUserId);
+router.get(
+  "/by-user/:userId",
+  checkRole("Admin", "Doctor", "Patient"),
+  patientController.getByUserId
+);
 
 // Отримати всіх пацієнтів конкретного лікаря (Doctor, Admin)
-router.get("/by-doctor/:doctorId", checkRole("Doctor", "Admin"), patientController.getByDoctor);
+router.get(
+  "/by-doctor/:doctorId",
+  checkRole("Doctor", "Admin"),
+  patientController.getByDoctor
+);
 
 // Get all patients (Admin, Doctor)
-router.get('/', checkRole('Admin', 'Doctor'), patientController.getAll);
+router.get("/", checkRole("Admin", "Doctor"), patientController.getAll);
 
 // Get one patient (Patient can view only self)
-router.get('/:id', patientController.getById);
+router.get("/:id", patientController.getById);
 
 // Create patient (Doctor or Admin)
-router.post('/', checkRole('Doctor', 'Admin'), patientController.create);
+router.post("/", checkRole("Doctor", "Admin"), patientController.create);
 
 // Update patient (Doctor, Admin, or Patient)
-router.put('/:id', checkRole('Doctor', 'Admin', 'Patient'), patientController.update.bind(patientController));
-
+router.put(
+  "/:id",
+  checkRole("Doctor", "Admin", "Patient"),
+  patientController.update.bind(patientController)
+);
 
 // Delete patient (Doctor, Admin)
-router.delete('/:id', checkRole('Doctor', 'Admin'), patientController.delete);
+router.delete("/:id", checkRole("Doctor", "Admin"), patientController.delete);
 
 module.exports = router;
