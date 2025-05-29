19c31d493678e032f3e911a81d46811511581918
Anastasiia Chernova <anastasiia.chernova@nure.ua>
2025-05-29 22:34:08 +0300

    Add route for updating doctor photo

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -1,21 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const doctorController = require('../controllers/doctorController');
-const authMiddleware = require('../middleware/authMiddleware');
-const checkRole = require('../middleware/checkRoleMiddleware');
+const doctorController = require("../controllers/doctorController");
+const authMiddleware = require("../middleware/authMiddleware");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
 router.use(authMiddleware);
 
-router.get('/specializations', doctorController.getUniqueSpecializations);
-router.get('/by-user/:userId', checkRole('Admin', 'Doctor'), doctorController.getDoctorByUserId);
-router.get('/', authMiddleware, doctorController.getAll);
-router.get('/:id', authMiddleware, doctorController.getById);
+router.put("/:id/photo", authMiddleware, doctorController.updatePhoto);
+router.get("/specializations", doctorController.getUniqueSpecializations);
+router.get(
+  "/by-user/:userId",
+  checkRole("Admin", "Doctor"),
+  doctorController.getDoctorByUserId
+);
+router.get("/", authMiddleware, doctorController.getAll);
+router.get("/:id", authMiddleware, doctorController.getById);
 
 router.get("/by-hospital/:hospitalId", doctorController.getByHospital);
 
 // ❗️тільки для Admin
-router.post('/', authMiddleware, checkRole('Admin'), doctorController.create);
-router.put('/:id', authMiddleware, checkRole('Admin'), doctorController.update);
-router.delete('/:id', authMiddleware, checkRole('Admin'), doctorController.delete);
+router.post("/", authMiddleware, checkRole("Admin"), doctorController.create);
+router.put("/:id", authMiddleware, checkRole("Admin"), doctorController.update);
+router.delete(
+  "/:id",
+  authMiddleware,
+  checkRole("Admin"),
+  doctorController.delete
+);
 
 module.exports = router;
